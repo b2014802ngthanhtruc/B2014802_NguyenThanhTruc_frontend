d588de99d65ec8deff5042e708177477a2ddd3d3
b2014802ngthanhtruc <trucb2014802@student.ctu.edu.vn>
2023-10-31 12:18:06 +0700

    Ket noi co so du lieu

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/merchandise.route");
-const ApiError = require("./app/api-error");
+//const contactsRouter = require("./app/routes/merchandise.route");
+//const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -12,16 +12,16 @@ app.get("/", (req, res) => {
     res.json({ message: "Chào mừng bạn đến với ứng dụng quản lý bán hàng"});
 });
 
-app.use("/api/merchandises", merchandisesRouter);
+// app.use("/api/merchandises", merchandisesRouter);
 
-app.use((req, res, next) => {
-    return next(new ApiError(404, "Resource not found"));
-});
+// app.use((req, res, next) => {
+//     return next(new ApiError(404, "Resource not found"));
+// });
 
-app.use((error, req, res, next) => {
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
-    });
-});
+// app.use((error, req, res, next) => {
+//     return res.status(error.statusCode || 500).json({
+//         message: error.message || "Internal Server Error",
+//     });
+// });
 
 module.exports = app;
